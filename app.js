4662786914b7d1f86d00f3599cc5ac53ffab6233
AnanyaCherukuri05 <ananyachrukuri5@gmail.com>
2025-05-12 10:29:35 +0530

    done

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 dotenv.config();
 
 app.get("/", async(req,res)=>{
-    res.send("Hello World")
+    res.send("Hello World!")
 })
 
 app.post("/register",async(req,res)=>{
@@ -24,11 +24,11 @@ app.post("/register",async(req,res)=>{
       if(password.length < 8 || password.length > 16){
         res.status(401).json({message:"Password length should be greater than 8 or less than or equal to 16"})
       }
-      
+
     } catch (error) {
         res.status(500).json({message:"Internal server error", error: error.message});
     }
-
+    res.status(201).json({message:"User registered sucessfully"})
 })
 
 
